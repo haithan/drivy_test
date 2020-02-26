b81801a169a0de6c6b7fbc253674316d8f7770de
Tuyen Tran <tuyen055@gmail.com>
2020-02-27 04:07:11 +0700

    finish level 3

---
+++ backend/core/rental.rb
@@ -0,0 +1,98 @@
+require 'date'
+
+class Rental
+  DEDUCE_RATE = [
+    { value: 1, rate: 10 },
+    { value: 4, rate: 30 },
+    { value: 10, rate: 50 }
+  ].freeze
+
+  attr_reader :id, :price, :insurance_fee, :assistance_fee, :commission,
+              :drivy_fee, :pricing_by_time, :pricing_by_range,
+              :rental_period, :start_date, :end_date, :distance,
+              :car_id, :car
+  def initialize(data, cars)
+    @id = data[:id]
+    @car_id = data[:car_id]
+    @start_date = data[:start_date]
+    @end_date = data[:end_date]
+    @distance = data[:distance]
+    @car = cars.select { |car| car[:id] == @car_id }.first
+    @rental_period = rental_period
+    @pricing_by_time = calculate_with_time
+    @pricing_by_range = calculate_with_range
+  end
+
+  def to_json
+    default_json
+    default_json.merge!(commission_data_json) unless @commission.nil?
+  end
+  
+  def calculate_price(is_deduce = false)
+    @price =  @pricing_by_time + @pricing_by_range
+    price_after_deduce if is_deduce
+  end
+  
+  def calculate_commission
+    @commission = (@price * 30.to_f/ 100).to_i
+    @insurance_fee = (commission * 50.to_f/ 100).to_i
+    @assistance_fee = 100 * rental_period
+    @drivy_fee = @commission - @insurance_fee - @assistance_fee
+  end
+
+  def payment
+    
+  end
+  
+  
+  private
+
+  def price_after_deduce
+    @price = @price - deduce_price
+  end
+
+  def deduce_price
+    period = @rental_period
+    price = 0
+    DEDUCE_RATE.reverse_each do |e|
+      next if e[:value] > period
+      actual_day = period - e[:value]
+      period = period - actual_day
+      price += (@car[:price_per_day] * (e[:rate].to_f/100) * actual_day)
+    end
+    price
+  end
+
+  def calculate_with_time
+    @car[:price_per_day] * @rental_period
+  end
+
+  def calculate_with_range
+    @car[:price_per_km] * @distance
+  end
+
+  def rental_period
+    converted_date(@end_date) - converted_date(@start_date) + 1
+  end
+  
+  def converted_date(date_str)
+    Date.parse(date_str).mjd
+  end
+
+  def default_json
+    {
+      id: @id,
+      price: @price.to_i
+    }
+  end
+
+  def commission_data_json
+    {
+      commission: {
+        insurance_fee: @insurance_fee,
+        assistance_fee: @assistance_fee,
+        drivy_fee: @drivy_fee
+      }
+    }
+  end
+end
--- backend/level2/main.rb
+++ backend/level2/main.rb
@@ -1,9 +1,19 @@
 require_relative '../core/util'
-require_relative '../core/calculator'
+require_relative '../core/rental'
+require 'json'
 
 util = Util.new("level2")
 
-json_data = util.read_file
-result_data = Calculator.new(json_data).to_receipt(is_deduce: true)
+json_data = JSON.parse(util.read_file , symbolize_names: true)
+cars_json = json_data[:cars]
+rentals_json = json_data[:rentals]
+    receipts = []
+    rentals_json.each do |rental|
+      receipt = Rental.new(rental, cars_json)
+      receipt.calculate_price(is_deduce: true)
+      receipts << receipt.to_json
+    end
+    { rentals: receipts }
+result_data = { rentals: receipts }
 
 util.write_file(result_data)
